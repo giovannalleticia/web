fdc1f1b1289cbc30f52c93d2a5a14dc7f0a386e3
leticia <giovannaleticia02203@gmail.com>
2024-12-03 09:49:49 -0300

    commit

---
+++ web_0312/src/menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Menu() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/sobre">Sobre</Link></li>
+            </ul>
+        </nav>
+    )
+}
