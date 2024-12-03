f1fc49c068db32d999c70d887ce7175ba2d2b2bb
leticia <giovannaleticia02203@gmail.com>
2024-12-03 10:17:26 -0300

    commit

--- web_0312/src/App.js
+++ web_0312/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './Home';
-import Home from './menu';
+import Home from './Menu';
 import Sobre from './Sobre';
 
 function App() {
